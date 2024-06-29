a7c8af431b011ac9bbca133b88ea39e12a5c4356
Peter Jones <pjones@devalot.com>
2024-06-28 20:54:04 -0700

    theme: Slight tweaks to the theme

--- pkgs/theme/build.rb
+++ pkgs/theme/build.rb
@@ -34,7 +34,7 @@ class Build
   def gen_sway
     colors = {
       # class                    border     bground   text      indicator child_border
-      "client.focused":          ["base0D", "base0D", "base00", "base0F", "base0D"],
+      "client.focused":          ["base0D", "base0D", "base00", "base03", "base0D"],
       "client.focused_inactive": ["base01", "base01", "base05", "base03", "base01"],
       "client.unfocused":        ["base01", "base00", "base05", "base03", "base01"],
       "client.urgent":           ["base08", "base08", "base00", "base08", "base08"],
