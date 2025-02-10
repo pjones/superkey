7e79575721cc44d776d7d6084e0bdc8385acd5f2
Peter Jones <pjones@devalot.com>
2025-02-10 10:01:49 +0100

    presenter: Map tablet tool to the external monitor

--- pkgs/presenter-mode/toggle-presenter-mode.sh
+++ pkgs/presenter-mode/toggle-presenter-mode.sh
@@ -53,6 +53,29 @@ toggle_inhibit() {
 }
 
 ################################################################################
+toggle_tablet_tool() {
+  local enabled=$1
+  local tool_id
+  local other_monitor="*"
+
+  tool_id=$(
+    swaymsg --type get_inputs |
+      jq --raw-output '.[] | select(.type=="tablet_tool") | .identifier' |
+      head -1
+  )
+
+  if [ "$enabled" = "false" ]; then
+    other_monitor=$(
+      swaymsg --type get_outputs |
+        jq --raw-output '.[] | select(.active and .name != "eDP-1") | .name' |
+        head -1
+    )
+  fi
+
+  swaymsg --type command "input $tool_id map_to_output $other_monitor"
+}
+
+################################################################################
 main() {
   local state=false
   local con_id=0
@@ -69,12 +92,14 @@ main() {
     set_val con-id "$con_id"
     toggle_dnd "$state"
     toggle_inhibit "$state" "$con_id"
+    toggle_tablet_tool "$state"
   else
     con_id=$(get_val con-id)
     set_val enabled false
     set_val con-id 0
     toggle_dnd "$state"
     toggle_inhibit "$state" "$con_id"
+    toggle_tablet_tool "$state"
   fi
 }
 
